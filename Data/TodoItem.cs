215596094f475d2b67f1385026b74ed5e8c86408
Aldin296 <99395958+Aldin296@users.noreply.github.com>
2023-11-18 16:13:25 +0100

    Initial
    Initial commit, planer und prototyp fürs kanban board

---
+++ Data/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace InVision.Data
+{
+	public class TodoItem
+	{
+		public string Title { get; set; }
+		public string? Description { get; set; }
+		public bool IsDone { get; set; }
+		public bool IsNew { get; set; }
+	}
+}

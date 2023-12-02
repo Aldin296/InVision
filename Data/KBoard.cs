960307f1ca69a84dedfa467a4834bfb2c21d1c6f
Aldin296 <99395958+Aldin296@users.noreply.github.com>
2023-12-02 23:35:58 +0100

    Probieren geht über studieren

--- Data/KBoard.cs
+++ Data/KBoard.cs
@@ -5,5 +5,7 @@
         public string Name {  get; set; }
         public string Description { get; set; }
         public string CreatedBy { get; set; }
+
+        public List<List<TodoItem>> ItemLists { get; set; }
     }
 }
--- Data/KBoardService.cs
+++ Data/KBoardService.cs
@@ -2,6 +2,31 @@
 {
     public class KBoardService
     {
+        List<List<TodoItem>> todoItems = new List<List<TodoItem>>()
+        {
+            new List<TodoItem>()
+            {
+                new TodoItem(){Title = "Item 1", Description="abc"},
+                new TodoItem(){Title = "Item 2", Description="abc"},
+                new TodoItem(){Title = "Item 3", Description="abc"},
+                new TodoItem(){Title = "Item 4", Description="444442"},
+                new TodoItem(){Title = "Item 5", Description="3a4bc"},
+                new TodoItem(){Title = "Item 7", Description="ad2bc"},
+            },
+            new List<TodoItem>()
+            {
+                new TodoItem(){Title = "Item 8", Description="ad a dw2bc"},
+                new TodoItem(){Title = "Item 9", Description="abc"},
+                new TodoItem(){Title = "Item 10", Description="abcde"},
+            },
+            new List<TodoItem>()
+            {
+                new TodoItem(){Title = "Item 11", Description="adsa fas faw bc"},
+                new TodoItem(){Title = "Item 12", Description="a sdf asbc"},
+                new TodoItem(){Title = "Item 13", Description="abcde"},
+            }
+        };
+
         List<KBoard> boards = new List<KBoard>()
         {
             new KBoard(){Name="Projekt1", Description="hier wird beschrieben das in diesem Board verwaltet wird", CreatedBy="User1"},
@@ -12,9 +37,16 @@
             new KBoard(){Name="spas und gas", Description="LOL", CreatedBy="Rizzler"}
         };
 
+       
+
         public async Task<List<KBoard>> BoardList()
         {
             return await Task.FromResult(boards);
+        }
+
+        public KBoard GetKBoardByName(string name)
+        {
+            return boards.Where(board => board.Name == name).FirstOrDefault();
         }
     }
 }

5c3a7d989c40543609dde269049f8804b0ed477d
Aldin296 <99395958+Aldin296@users.noreply.github.com>
2023-12-19 16:14:09 +0100

    added Id as Guid

--- Data/KBoard.cs
+++ Data/KBoard.cs
@@ -2,10 +2,15 @@
 {
     public class KBoard
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name {  get; set; }
         public string Description { get; set; }
         public string CreatedBy { get; set; }
-        public List<TodoItem> Items { get; set; } = new List<TodoItem>();
+        public List<TodoItem>? Items { get; set; } = new List<TodoItem>();
+
+        public KBoard() 
+        {
+            Id = Guid.NewGuid().ToString();
+        }
     }
 }
--- Data/KBoardService.cs
+++ Data/KBoardService.cs
@@ -6,7 +6,7 @@
 
         List<KBoard> boards = new List<KBoard>()
         {
-            new KBoard(){Id=1, Name="Projekt1", Description="hier wird beschrieben das in diesem Board verwaltet wird", CreatedBy="User1", Items = { new TodoItem { Title="title", Description="ich tue dies und das", state=0}, new TodoItem {Title="ich mache etwas", Description="ich mache tatsachlich etwas lol", state=1 }, new TodoItem {Title="haha lol invision", Description="diplomarbeit für die schule ich ahbe fun", state=2} } },
+            new KBoard(){Name="Projekt1", Description="hier wird beschrieben das in diesem Board verwaltet wird", CreatedBy="User1", Items = { new TodoItem { Title="title", Description="ich tue dies und das", state=0}, new TodoItem {Title="ich mache etwas", Description="ich mache tatsachlich etwas lol", state=1 }, new TodoItem {Title="haha lol invision", Description="diplomarbeit für die schule ich ahbe fun", state=2} } },
             new KBoard(){Name="Weise Haus", Description="Ex Präsidenten Buisness", CreatedBy="Obama"},
             new KBoard(){Name="Weise Haus 2022", Description="Gelbe Präsidenten Buisness", CreatedBy="Trump"},
             new KBoard(){Name="InVision", Description="Diplomarbeit - WebbPlaner für Menschen ohne Plan", CreatedBy="Jan"},
@@ -24,7 +24,7 @@
             return boards.Where(board => board.Name == name).FirstOrDefault();
         }
 
-        public KBoard GetBoardById(int id)
+        public KBoard GetBoardById(string id)
         {
             return boards.Where(board => board.Id == id).FirstOrDefault();
         }

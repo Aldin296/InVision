734a06e2eb9e9ef4e452e68f8375c2cce9328052
Jan <57670746+Janoran@users.noreply.github.com>
2023-12-19 16:13:19 +0100

    InVision API
    Die API zur Radzenblazor app

---
+++ InVision_API/Models/InVisionDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace InVision_API.Models
+{
+    public class InVisionDatabaseSettings
+    {
+
+        public string ConnectionString { get; set; } = null;
+        public string DatabaseName { get; set; } = null;
+        public string CollectionName { get; set; } = null;
+    } 
+}
---
+++ InVision_API/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace InVision_API.Models
+{
+
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public List<Note>? Notes { get; set; } = null;
+		public List<KBoard>? KBoards { get; set; } 
+
+		public byte[] salt { get; set; } //Für die berechnung des hash passwortes, bzw. den Login
+
+    }
+}

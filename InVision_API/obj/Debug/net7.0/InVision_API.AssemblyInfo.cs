ff7dc8e57b87edebfad0b2a4b7b96f7307f9a3c4
Aldin296 <99395958+Aldin296@users.noreply.github.com>
2024-01-18 11:48:32 +0100

    Notes, searchkanban, new layout, new sidebar, responsive

--- InVision_API/obj/Debug/net7.0/InVision_API.AssemblyInfo.cs
+++ InVision_API/obj/Debug/net7.0/InVision_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InVision_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3a7d989c40543609dde269049f8804b0ed477d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("InVision_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InVision_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

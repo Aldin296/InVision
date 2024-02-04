c2e0ca12942dfb6c0215c0f04c3072af525c2795
Jan <57670746+Janoran@users.noreply.github.com>
2024-02-04 21:28:48 +0100

    Refactoring of Code
    Improved the Code by splitting code up in more methods as well as writing the first steps for saving the new changed password

---
+++ InVision/Data/HashPassword.cs
@@ -0,0 +1,18 @@
+﻿namespace InVision.Data
+{
+    public class HashPassword
+    {
+        public string? Hashed { get; set; }
+        public byte[]? Salt { get; set; }
+
+        public HashPassword()
+        {
+        }
+
+        public HashPassword(string? hashed, byte[]? salt)
+        {
+            Hashed = hashed;
+            Salt = salt;
+        }
+    }
+}

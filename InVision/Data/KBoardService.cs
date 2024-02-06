b930e8cc12ba6b1cf8d720bfe8e1277aad0555a6
Aldin296 <99395958+Aldin296@users.noreply.github.com>
2024-02-06 12:41:46 +0100

    bug fix: man kann in leere boards rin gehen

--- InVision/Data/KBoardService.cs
+++ InVision/Data/KBoardService.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using MongoDB.Driver;
 using System.Text.Json;
+using System.Xml.Linq;
 
 namespace InVision.Data
 
@@ -11,23 +13,21 @@ namespace InVision.Data
 		private readonly string baseurl = "https://localhost:7133";
 		HttpClient client = new HttpClient();
 		public KBoard selectedBoard { get;set; }
+		List<KBoard> boards = new List<KBoard>(){};
 
-        List<KBoard> boards = new List<KBoard>(){};
-        
-
-        /*public async Task<List<KBoard>> BoardList()
+		/*public async Task<List<KBoard>> BoardList()
         {
             return await Task.FromResult(boards);
         }*/
-        /*public async Task<KBoard?> GetKBoardAsync(string userId, string kboardId)
-        {
-            var user = await CurrentUser.Find(x => x.Id == userId).FirstOrDefaultAsync();
+			/*public async Task<KBoard?> GetKBoardAsync(string userId, string kboardId)
+			{
+				var user = await CurrentUser.Find(x => x.Id == userId).FirstOrDefaultAsync();
 
 
-            return user?.KBoards?.Find(kboard => kboard.Id == kboardId);
-        }*/
+				return user?.KBoards?.Find(kboard => kboard.Id == kboardId);
+			}*/
 
-        public KBoard GetKBoardByName(string name)
+		public KBoard GetKBoardByName(string name)
         {
             return boards.Where(board => board.Name == name).FirstOrDefault();
         }
